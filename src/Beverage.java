53b0b38afecf09b8d6a8a88903372ef9f119affd
CherylL23 <Cheryl.Lee23@ycdsbk12.ca>
2022-11-19 16:15:49 -0500

    Beverage class

--- src/Beverage.java
+++ src/Beverage.java
@@ -3,9 +3,14 @@ public abstract class Beverage {
     private String beverageName;
     private String beverageTemp;
     private String beverageSize;
-    private int beveragePrice;
+    private double beveragePrice;
 
-  
+    public Beverage(String aName, String aSize, String aTemp, double aPrice){
+        beverageName = aName;
+        beverageSize = aSize;
+        beverageTemp = aTemp;
+        beveragePrice = aPrice;
+    }
 
     
 
