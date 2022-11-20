8af862722a8009a6b26c53744e37f3d4989abdc6
CherylL23 <Cheryl.Lee23@ycdsbk12.ca>
2022-11-20 00:32:11 -0500

    donut class

--- src/GoldenDonuts/Donuts.java
+++ src/GoldenDonuts/Donuts.java
@@ -1,4 +1,14 @@
 package GoldenDonuts;
 public class Donuts {
     
+    private String donutName;
+    private double donutPrice;
+    private int donutNum;
+    
+    public Donut(String donutType, int anAmount, double thePrice){
+        donutName = donutType;
+        donutNum = anAmount;
+        donutPrice = thePrice;
+    }
+    
 }
