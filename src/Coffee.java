f540bbc41ee3eb41ce1b4a3f7ab89fe30f701ea0
CherylL23 <Cheryl.Lee23@ycdsbk12.ca>
2022-11-19 23:30:29 -0500

    recieve info on coffee

--- src/Coffee.java
+++ src/Coffee.java
@@ -20,8 +20,40 @@ public class Coffee extends Beverage{
         creamTop = creamOp;
     }
 
- 
+    public void recieveI(String[] args) throws Exception {
 
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("What size do you want? We have small, medium, and large.: ");
+        String strSize = reader.readLine();
+
+        if(strSize.equalsIgnoreCase("small")){
+            setSize(strSize);
+            setPrice(0.50);
+        }
+
+        if(strSize.equalsIgnoreCase("medium")){
+            setSize(strSize);
+            setPrice(1.00);
+        }
+
+        if(strSize.equalsIgnoreCase("large")){
+            setSize(strSize);
+            setPrice(1.50);
+        }
+
+        System.out.println("Do you want whipped cream? Yes or no?: ");
+        String strCream = reader.readLine();
+
+        if(strCream.equalsIgnoreCase("yes")){
+            setCream(true);
+        }
+
+        if(strCream.equalsIgnoreCase("no")){
+            setCream(false);
+        }
+    }
+
+    
 
 
 }
