eb8d8186e37f133ba52bdb9b3e2f1974d64de94b
CherylL23 <Cheryl.Lee23@ycdsbk12.ca>
2022-11-19 22:40:27 -0500

    coffee class

--- src/Coffee.java
+++ src/Coffee.java
@@ -1,5 +1,13 @@
 
 public class Coffee extends Beverage{
 
-   
+    boolean creamTop;
+
+    public Coffee(String aName, String aSize, String aTemp, double aPrice, boolean whippedCream){
+
+        super(aName, aSize, aTemp, aPrice);
+        creamTop = whippedCream;
+    }
+
+
 }
