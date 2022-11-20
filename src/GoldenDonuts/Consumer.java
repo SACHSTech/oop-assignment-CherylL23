fe03ab0949d5aa6bc2ad39277eae75d88d099c4a
CherylL23 <Cheryl.Lee23@ycdsbk12.ca>
2022-11-20 12:16:55 -0500

    cosumer class

--- src/GoldenDonuts/Consumer.java
+++ src/GoldenDonuts/Consumer.java
@@ -1,4 +1,19 @@
 package GoldenDonuts;
 public class Consumer {
     
+    private String conName;
+    private int creditCard;
+    private String conEmail;
+    private String conPhone;
+
+    Consumer(String userName, String userEmail, String phoneNum, int cardNum){
+        conName = userName;
+        conEmail = userEmail;
+        conPhone = phoneNum;
+        creditCard = cardNum;
+    }
+
+    
+
+
 }
