bdd7f06c0e9fec4a203c8e412bcfa1351c789991
CherylL23 <Cheryl.Lee23@ycdsbk12.ca>
2022-11-29 17:52:52 -0500

    ordering donuts

--- src/GoldenDonuts/Order.java
+++ src/GoldenDonuts/Order.java
@@ -1,12 +1,22 @@
 package GoldenDonuts;
+import java.util.*;
 public class Order {
     
     private double subTotal;
     private double finalTotal;
     private Consumer consumer;
+    private ArrayList<Donuts> donutList = new ArrayList<Donuts>();
+    private ArrayList<Beverage> beverageList = new ArrayList<Beverage>();
     
     public Order(){
         consumer = new Consumer();;
     }
+
+    public void addDonut(Donuts newDonut){
+        donutList.add(newDonut);
+
+    }
+
+    
 
 }
